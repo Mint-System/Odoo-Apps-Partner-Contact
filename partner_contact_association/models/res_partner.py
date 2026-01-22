a408f0f1b5192d77507c50ae7d8e2841d563d359
Janik von Rotz <login@janikvonrotz.ch>
2026-01-22 09:16:53 +0100

    feat(partner_contact_association): expand _rec_names_search to include standard fields

--- partner_contact_association/models/res_partner.py
+++ partner_contact_association/models/res_partner.py
@@ -7,7 +7,7 @@ _logger = logging.getLogger(__name__)
 
 class Partner(models.Model):
     _inherit = "res.partner"
-    _rec_names_search = ["association_name"]
+    _rec_names_search = ["complete_name", "email", "ref", "vat", "company_registry", "association_name"]
 
     association_id = fields.Many2one("res.association")
     association_name = fields.Char(related="association_id.name", string="Assocation Name", store=True)
