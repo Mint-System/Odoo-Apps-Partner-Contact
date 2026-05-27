23f13a6d8426982939efc7451cd96e4452fee3d2
Janik von Rotz <login@janikvonrotz.ch>
2026-05-27 11:45:23 +0200

    feat: add comment field to partner link models and improve German translations

--- partner_link/models/res_partner_link.py
+++ partner_link/models/res_partner_link.py
@@ -13,6 +13,8 @@ class ResPartnerLink(models.Model):
 
     label_id = fields.Many2one("res.partner.link.label", required=True)
     partner_id = fields.Many2one("res.partner", string="Contact", required=True)
+    comment = fields.Char()
+
     source_partner_id = fields.Many2one("res.partner", string="Contact")
 
     def _compute_display_name(self):
